80699868dab3df3b660951046fbd27970e163817
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-12-01 07:21:31 +1100

    shoecard: hanlder function added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
 import { shoes, statistics } from "../constants";
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
@@ -44,16 +46,23 @@ const Hero = () => {
       {/* Hero image */}
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-        <div>
+        {/* thumbnails */}
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe, index) => (
             <div key={index}>
-              <ShoeCard />
+              <ShoeCard
+                imgUrl={shoe}
+                changeBigShoeImage={(shoe) => {
+                  setBigShoeImg(shoe);
+                }}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
